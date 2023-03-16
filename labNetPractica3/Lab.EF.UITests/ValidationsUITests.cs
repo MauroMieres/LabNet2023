a6a6333f98e74258fb30c13f22a716423dcba2a1
MauroMieres <mauronicolasmieres@gmail.com>
2023-03-16 09:51:27 -0300

    Agregados Unit Test

---
+++ labNetPractica3/Lab.EF.UITests/ValidationsUITests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab.EF.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.EF.UI.Tests
+{
+    [TestClass()]
+    public class ValidationsUITests
+    {
+        [TestMethod()]
+        [DataRow("'¿'¿''¿")]
+        [DataRow("texto")]
+        [DataRow("")]
+        public void ValidateYesNoInputTestInvalidInput(string input)
+        {
+            Assert.IsFalse(ValidationsUI.ValidateYesNoInput(input));
+        }
+
+        [TestMethod()]
+        [DataRow("blablablablabla",7)]
+        public void ValidateStringLengthTestInputIsTooLong(string input, int maxLength)
+        {
+            Assert.IsFalse(ValidationsUI.ValidateStringLength(input, maxLength));
+        }
+
+        [TestMethod()]
+        [DataRow("blablablablabla", 20)]
+        public void ValidateStringLengthTestInputIsOk(string input, int maxLength)
+        {
+            Assert.IsTrue(ValidationsUI.ValidateStringLength(input, maxLength));
+        }
+    }
+}
