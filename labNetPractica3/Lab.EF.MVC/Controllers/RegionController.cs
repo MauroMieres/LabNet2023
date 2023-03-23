155f7523be0be0332cf382c41e295c5e25fc227c
MauroMieres <mauronicolasmieres@gmail.com>
2023-03-23 15:52:09 -0300

    Base terminada

---
+++ labNetPractica3/Lab.EF.MVC/Controllers/RegionController.cs
@@ -0,0 +1,52 @@
+﻿using Lab.EF.Entities;
+using Lab.EF.Logic;
+using Lab.EF.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab.EF.MVC.Controllers
+{
+    public class RegionController : Controller
+    {
+        // GET: Region
+        public ActionResult Index()
+        {
+            IABMLogic<Region> regionesLogic = new RegionLogic();
+            var regiones = regionesLogic.GetAll().Select(r => new RegionViewModel { Id = r.RegionID, Descripcion = r.RegionDescription }).ToList();
+            return View(regiones);
+        }
+
+        public ActionResult Eliminar(int id) 
+        {
+            IABMLogic<Region> regionesLogic = new RegionLogic();
+            var entity = regionesLogic.GetById(id);
+            regionesLogic.Delete(entity);
+            return null;
+        }
+
+        public ActionResult Modificar(int id)
+        {
+            IABMLogic<Region> regionesLogic = new RegionLogic();
+            RegionViewModel model = new RegionViewModel();
+            var entity = regionesLogic.GetById(id);
+            model.Id = entity.RegionID;
+            model.Descripcion = entity.RegionDescription;
+            //regionesLogic.Update(entity);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Modificar(RegionViewModel model)
+        {
+            //falta validacion
+            IABMLogic<Region> regionesLogic = new RegionLogic();
+            regionesLogic.Update(new Region { RegionID= model.Id, RegionDescription=model.Descripcion});
+
+            //manejo de errores
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ labNetPractica3/Lab.EF.MVC/Models/RegionViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab.EF.MVC.Models
+{
+    public class RegionViewModel
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
